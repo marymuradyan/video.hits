93ab4572a34b57ae4dcd395d50aa6d2d6ca7da0e
Mary <muradyan.ip@gmail.com>
2015-09-14 20:09:48 +0300

    1 commit

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=video.hits',
+    'username' => 'root',
+    'password' => '//baci//',
+    'charset' => 'utf8',
+];
---
+++ config/web.php
@@ -0,0 +1,83 @@
+<?php
+
+$params = require(__DIR__ . '/params.php');
+
+$config = [
+    'id' => 'basic',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'timeZone' =>'Europe/Moscow',
+    'components' => [
+        'urlManager' => [
+          
+          'enablePrettyUrl' => true,
+          'showScriptName' => false,
+         /* 'rules' => [
+            '<controller>/<action>' => '<controller>/<action>',
+            'site/index/<name:\w+>' => 'site/index',
+          ]
+         */ 
+        ],
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'MtVcmXjiebGlowgrItgKytNdaj4UfGmX',
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'user' => [
+            'identityClass' => 'app\models\User',
+            'enableAutoLogin' => true,
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+            'fileTransportPath' => '/tmp/mail.txt'
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'db' => require(__DIR__ . '/db.php'),
+        
+        
+        'authClientCollection' => [
+            'class' => 'yii\authclient\Collection',
+            'clients' => [
+                'facebook' => [
+                    'class' => 'yii\authclient\clients\Facebook',
+                    'clientId' => '847251218658947',
+                    'clientSecret' => '04a3e16b464e3c1a4b730a26bee85afb',
+                ],
+            ],
+        ],
+        
+    ],
+    'params' => $params,
+];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
